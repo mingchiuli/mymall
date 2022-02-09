9cc13e584377c7ea494887334919e106fe2846a2
mingchiuli <mingchiuli@163.com>
2022-02-10 01:23:13 +0800

    gateway学习

---
+++ mymall-gateway/src/main/java/com/atguigu/mymall/gateway/MymallGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.mymall.gateway;
+
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@EnableDiscoveryClient
+public class MymallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallGatewayApplication.class, args);
+    }
+
+}
---
+++ mymall-gateway/src/test/java/com/atguigu/mymall/gateway/MymallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.mymall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
